c8556514de4a050542eaab2142be74daef1ce8cf
Paul <pawel.sroka0@gmail.com>
2024-07-01 10:14:15 +0200

    Update scrap.R

--- scrap.R
+++ scrap.R
@@ -33,6 +33,10 @@ for(type_ in c('atp', 'wta')){
   tns_schedule = rbind(tns_schedule, tb_schedule_)
 }
 
+  print('new test')
+  rD <- rsDriver(browser = "firefox", port = 4444L, verbose = FALSE)
+  remote_driver <- rD[["client"]]
+  print('after new test')
   print(.Platform$OS.type) 
   
   # scrap_start_session()
