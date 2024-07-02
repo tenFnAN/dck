632f2a6a0cac970671f6b84ea516f566f83110b4
Paul <pawel.sroka0@gmail.com>
2024-07-02 22:24:00 +0200

    Update scrap_.R

--- scrap_.R
+++ scrap_.R
@@ -1,6 +1,8 @@
 #load libs
 library(dplyr)  ; library(collapse)   
 library(rvest)  
+library(RSelenium)
+source('functions.R')
 #library(dplyr)  ; library(collapse)  ; library(data.table); library(purrr) ; library(lubridate)
 #library(rvest)  ; library(polite)    ; library(stringr)   ; library(readr)
 print('inside script')
