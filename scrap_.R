59de1228d3379e2ec8591afbb4a7830b63eba033
Paul <pawel.sroka0@gmail.com>
2024-07-02 22:39:56 +0200

    Update scrap_.R

--- scrap_.R
+++ scrap_.R
@@ -6,7 +6,7 @@ source('/home/seluser/functions.R')
 #library(dplyr)  ; library(collapse)  ; library(data.table); library(purrr) ; library(lubridate)
 #library(rvest)  ; library(polite)    ; library(stringr)   ; library(readr)
 print('inside script')
-
+print(R.version)
 
 
  for(type_ in c('atp', 'wta')){
