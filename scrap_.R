bada0c054183e79fd72a3be8934309699a6a175d
Paul <pawel.sroka0@gmail.com>
2024-07-02 14:22:24 +0200

    Update scrap_.R

--- scrap_.R
+++ scrap_.R
@@ -1,7 +1,11 @@
 #load libs
-library(dplyr)  ; library(collapse)   ; library(data.table)
+library(dplyr)  ; library(collapse)   ; library(data.table) ; library(purrr)
 
 #library(dplyr)  ; library(collapse)  ; library(data.table); library(purrr) ; library(lubridate)
 #library(rvest)  ; library(polite)    ; library(stringr)   ; library(readr)
 print('inside script')
+
+
+
  
+print('end script')
