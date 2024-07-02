820c57b1fdf32177168cf57ef9d4d34458e5f7aa
Paul <pawel.sroka0@gmail.com>
2024-07-02 14:26:13 +0200

    Update scrap_.R

--- scrap_.R
+++ scrap_.R
@@ -1,6 +1,6 @@
 #load libs
 library(dplyr)  ; library(collapse)   ; library(data.table) ; library(purrr) ; library(lubridate)
-library(rvest)  ; library(polite) 
+library(rvest)  
 #library(dplyr)  ; library(collapse)  ; library(data.table); library(purrr) ; library(lubridate)
 #library(rvest)  ; library(polite)    ; library(stringr)   ; library(readr)
 print('inside script')
