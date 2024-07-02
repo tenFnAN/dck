f0a0a8ce014cedecd53c5c8ba1ed9a5f419ff8c9
Paul <pawel.sroka0@gmail.com>
2024-07-02 22:31:49 +0200

    Update scrap_.R

--- scrap_.R
+++ scrap_.R
@@ -1,6 +1,6 @@
 #load libs
 library(dplyr)  ; library(collapse)   
-library(rvest)  
+library(rvest)  ; library(polite)
 library(RSelenium)
 source('/home/seluser/functions.R')
 #library(dplyr)  ; library(collapse)  ; library(data.table); library(purrr) ; library(lubridate)
@@ -9,5 +9,28 @@ print('inside script')
 
 
 
- 
+ for(type_ in c('atp', 'wta')){
+  tb_schedule = get_schedule( url = str_glue('https://live-tennis.eu/en/{type_}-schedule') ) 
+  
+  tb_schedule_ = tb_schedule %>%
+    reshape2::melt('Player_Name_1') %>%
+    fmutate( Data = ymd(paste0(substr(variable, 13, 16), substr(variable, 7, 8), substr(variable, 10, 11) )),
+             Position     = '',
+             Priority     = '1',
+             Information = ifelse(str_detect(value, 'Qual'), 'QUALIFYING', 'MAIN DRAW'),
+             Kraj         = '',
+             Nawierzchnia = '' ) %>%
+    frename(Player = Player_Name_1, Turniej = value) %>%
+    fsubset(Player != 'player name') %>%
+    fselect(-variable) %>%
+    fmutate(update = as.Date(Sys.time())) %>%
+    fsubset(!Turniej %in% c('-', '') & Player != '(available slot)') %>%
+    fmutate(Turniej = as.factor(Turniej)) %>%
+    roworderv(c('Player', 'Data')) %>%
+    fmutate(type = type_) %>%
+    select(type, everything())
+  
+  tns_schedule = rbind(tns_schedule, tb_schedule_)
+}
+
 print('end script')
